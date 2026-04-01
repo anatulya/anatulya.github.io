8aa5928fa62b56ad08222e41b69cba2edd3cb0a3
anatulya <anatulya.nandi@gmail.com>
2026-04-01 13:24:20 -0500

    testing no base path

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/anatulya.github.io",
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
